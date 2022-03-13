514a898cbfd8f70eeede6da134e23b2bda05538f
Eva813 <as45986@gmail.com>
2022-03-13 17:05:39 +0800

    set socket

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,4 +1,14 @@
 import "./index.css";
 import { name } from "@/utils";
+//將 socket.io 的包引入前端程式這邊
+import { io } from "socket.io-client";
+
+//1. 建立連接到 node server
+const clientIo = io();
+//建立連接成功，接收後端發過來的訊息
+clientIo.on('join', (msg) => {
+  console.log('msg', msg)
+})
+
 
 console.log("client side chatroom page", name);
--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,18 @@ import http from "http"
 
 import { name } from "@/utils";
 
+
 const port = 3000;
 const app = express();
+//透過 http 將 server 建立起來
+const server = http.createServer(app)
+const io = new Server(server)
+
+//當有用戶連接到connection 就會有回呼函式
+//2. 監測連接
+io.on('connection', (socket) => {
+  socket.emit('join', 'Welcom')
+})
 
 // 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
 if (process.env.NODE_ENV === "development") {
@@ -18,6 +28,6 @@ if (process.env.NODE_ENV === "development") {
 
 console.log("server side", name);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`The application is running on port ${port}.`);
 });
