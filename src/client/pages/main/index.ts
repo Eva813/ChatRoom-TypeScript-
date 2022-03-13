909170b3cf12dafda23150d3149f9509b2168edc
Eva813 <as45986@gmail.com>
2022-03-13 20:03:54 +0800

    get element and add click event

--- src/client/pages/main/index.ts
+++ src/client/pages/main/index.ts
@@ -1,4 +1,20 @@
 import "./index.css";
 import { name } from "@/utils";
 
-console.log("client side main page", name);
+//取得DOM 元素
+const nameInput = document.getElementById("nameInput") as HTMLInputElement;
+const nameSelect = document.getElementById("nameSelect") as HTMLSelectElement;
+const startBtn = document.getElementById("startBtn") as HTMLButtonElement;
+
+//拿
+startBtn.addEventListener("click", () => {
+  const userName = nameInput.value;
+  const roomName = nameSelect.value;
+  console.log('userName', userName, roomName);
+  //跳轉到該頁面 > 查看 dev.ts 中的路徑名
+  location.href = `/chatRoom/chatRoom.html?user_name=${userName}&room_name=${roomName}`
+  //location.href = `/chatRoom`
+})
+
+
+//在按鈕加入事件
