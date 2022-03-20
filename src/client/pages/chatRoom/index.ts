abbb7ab90f1b83f5d32950b1c3c12b7bdadaa9b0
Eva813 <as45986@gmail.com>
2022-03-20 09:09:26 +0800

    get url quwrry string

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -3,6 +3,18 @@ import { name } from "@/utils";
 //將 socket.io 的包引入前端程式這邊
 import { io } from "socket.io-client";
 
+//在進入頁面時，從url 拿到 querry string 的資訊
+//使用 window 的API
+const url = new URL(location.href)
+const userName = url.searchParams.get('user_name')
+const roomName = url.searchParams.get('room_name')
+
+//加入沒有取得資訊，要跳轉登入頁
+if (!userName || !roomName) {
+  location.href = "/main/main.html"
+}
+console.log(userName, roomName)
+
 //1. 建立連接到 node server
 const clientIo = io();
 //建立連接成功，接收後端發過來的訊息
